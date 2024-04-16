d73e412f3860add22dde63ad5272feafd6f0589e
NorbertNagy12345 <norbert.nagy.comp@gmail.com>
2024-04-16 19:56:59 +0300

    commit

--- src/main/java/org/fasttrackit/curs20/service/SensorService.java
+++ src/main/java/org/fasttrackit/curs20/service/SensorService.java
@@ -5,10 +5,25 @@ import org.fasttrackit.curs20.model.Room;
 import org.fasttrackit.curs20.repository.RoomRepository;
 
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
 
 public class SensorService {
+    private final ScheduledExecutorService scheduler;
     RoomRepository roomRepository;
+
+    public SensorService() {
+        scheduler = Executors.newScheduledThreadPool(1);
+        scheduler.scheduleAtFixedRate(this::randomWaterSensorState, 0, 10, TimeUnit.MINUTES);
+        scheduler.scheduleAtFixedRate(this::randomSmokeSensorState, 0, 10, TimeUnit.MINUTES);
+        scheduler.scheduleAtFixedRate(this::randomGasSensorState, 0, 10, TimeUnit.MINUTES);
+    }
+
+    public void stopScheduler() {
+        scheduler.shutdown();
+    }
 
     public void randomSmokeSensorState() {
         int randomNum = ThreadLocalRandom.current().nextInt(0, 5);
